5474c007849cff347bc76ab82775806363ed244b
Eksezis <official.eksezis@gmail.com>
2026-03-12 10:30:09 +0100

    Updated StarterPlayer/StarterPlayerScripts/Client/Initializer.lua

---
+++ StarterPlayer/StarterPlayerScripts/Client/Initializer.lua
@@ -0,0 +1,7 @@
+-- @ScriptType: LocalScript
+for _,v in game.ReplicatedStorage.Resources.Movement:GetChildren() do
+	if v:IsA("ModuleScript") then
+		local module = require(v)
+		module:init()
+	end
+end
